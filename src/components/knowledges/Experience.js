d15f4e54919b48bc4015eac8dbd6506685ba13fd
Miguigno <lemairemarius>
2021-07-28 16:40:13 +0200

    knowledges

---
+++ src/components/knowledges/Experience.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Experience = () => {
+    return (
+        <div className="experience">
+            <h3>Expériences</h3>
+            <div className="exp1">
+                <h4>diecteur</h4>
+                <h5>2018-2020</h5>
+                <p>Lorem ipsum</p>
+            </div>
+            <div className="exp-2">
+                <h4>blabla</h4>
+                <h5>2190-2504</h5>
+                <p>lorem ipsum</p>
+            </div>
+        </div>
+    );
+}
+
+export default Experience;
--- src/pages/Knowledges.js
+++ src/pages/Knowledges.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import Navigation from "../components/Navigation";
+import Languages from "../components/knowledges/Languages";
+import Experience from "../components/knowledges/Experience";
+import OtherSkills from "../components/knowledges/OtherSkills";
+import Hobbies from "../components/knowledges/Hobbies";
 
 const Knowledges = () => {
     return (
-        <div>
+        <div className="knowledges">
             <Navigation />
-            knowledges !!
+            <div className="knowledgesContent">
+                <Languages />
+                <Experience />
+                <OtherSkills />
+                <Hobbies />
+
+            </div>
         </div>
     );
 };
